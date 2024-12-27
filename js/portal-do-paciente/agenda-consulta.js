d1f0d792f9019ef5793f7e2c13e3139716958b14
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-26 21:31:52 -0300

    fix: correção do bug no input de data; correção de autorização nas chamadas dos endpoints;

--- js/portal-do-paciente/agenda-consulta.js
+++ js/portal-do-paciente/agenda-consulta.js
@@ -1,36 +1,34 @@
-
-const avancarButton = document.getElementById('avancar');
-const voltarButton = document.getElementById('voltar');
 const formSelecao = document.getElementById('form-selecao');
 const resumoConsultaDiv = document.getElementById('resumo-consulta');
-const valorConsultaText = document.getElementById('valor-consulta');
-const enderecoConsultaText = document.getElementById('endereco-consulta');
-const medicoConsultaText = document.getElementById('medico-consulta');
-const especialidadeConsultaText = document.getElementById('especialidade-consulta');
-const dataConsultaText = document.getElementById('data-consulta');
-const horaConsultaText = document.getElementById('horario-consulta');
 const especialidadeSelect = document.getElementById('especialidade');
 const dataInput = document.getElementById('data');
 const horarioSelect = document.getElementById('horario');
 const medicoSelect = document.getElementById('medico');
 const resumoConsulta = document.getElementById('resumo-consulta');
+const descricaoInput = document.getElementById('descricao');
+const isConsultaOnline = document.getElementById('online');
 
-const hoje = new Date();
-const ano = hoje.getFullYear();
-const mes = String(hoje.getMonth() + 1).padStart(2, '0'); // Meses são baseados em zero
-const dia = String(hoje.getDate()).padStart(2, '0');
-const dataAtual = `${ano}-${mes}-${dia}`;
+document.addEventListener('DOMContentLoaded', async () => {
+  const token = sessionStorage.getItem("token")
 
-document.addEventListener('DOMContentLoaded', carregarMedicos)
+  const acesso = await fetch('http://localhost:8080/api/Consulta/Acessar', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+  }});
 
+  if (acesso.status == 401) 
+      window.location.href = "http://127.0.0.1:5500/html/login.html"
 
-
-async function carregarMedicos() {
   const response = await fetch('http://localhost:8080/api/Medico/ListarTodos');
   const medicos = await response.json();
 
+  const hoje = new Date();
+  dataInput.min = retornaDataFormatada(hoje);
+
   const urlParams = new URLSearchParams(window.location.search);
   const medicoQuery = urlParams.get('medico');
+
 
   medicoSelect.innerHTML = '<option value="">Selecione um médico</option>';
   medicos.forEach(medico => {
@@ -44,16 +42,45 @@ async function carregarMedicos() {
       carregarEspecialidades(medico.cpf);
     }
   });
-}
+})
+
+document.getElementById('avancar').addEventListener('click', () => {
+  if(!especialidadeSelect.value || !medicoSelect.value || !dataInput.value || !horarioSelect.value){
+    alert("Insira todos os campos!");
+    return;
+  }
+  const cpf = medicoSelect.value;
+  if (cpf) {
+    carregarResumo(cpf);
+    formSelecao.classList.remove('form-active');
+    resumoConsultaDiv.classList.add('form-active');
+  }
+});
+
+document.getElementById('voltar').addEventListener('click', () => {
+  resumoConsultaDiv.classList.remove('form-active');
+  formSelecao.classList.add('form-active');
+});
+
+medicoSelect.addEventListener('change', () => {
+  const cpf = medicoSelect.value;
+  if (cpf) 
+    carregarEspecialidades(cpf);
+  else 
+    especialidadeSelect.innerHTML = '<option value="">Selecione um médico primeiro</option>';
+});
 
 async function carregarEspecialidades(cpf) {
+  
+  dataInput.value = '';
+
   const response = await fetch(`http://localhost:8080/api/Especialidade/obterEspecialidadesMedico?cpf=${cpf}`);
   const especialidades = await response.json();
 
   especialidadeSelect.innerHTML = '<option value="">Selecione uma especialidade</option>';
   especialidades.forEach(esp => {
     const option = document.createElement('option');
-    option.setAttribute('especialidade-id', esp.especialidadeId);
+    option.id = esp.especialidadeId
     option.value = esp.nome;
     option.textContent = esp.nome;
     especialidadeSelect.appendChild(option);
@@ -61,8 +88,7 @@ async function carregarEspecialidades(cpf) {
 }
 
 async function carregarDisponibilidades(cpf, data) {
-  const formattedData = data.split('-').reverse().join('/');
-  const response = await fetch(`http://localhost:8080/api/Disponibilidade/listarDisponibilidadesMedicoPorData?cpf=${cpf}&data=${formattedData}`);
+  const response = await fetch(`http://localhost:8080/api/Disponibilidade/listarDisponibilidadesMedicoPorData?cpf=${cpf}&data=${data}`);
   const disponibilidades = await response.json();
   
   if(disponibilidades.length == 0){
@@ -73,25 +99,16 @@ async function carregarDisponibilidades(cpf, data) {
   horarioSelect.innerHTML = '<option value="">Selecione um horário</option>';
   disponibilidades.forEach(disponibilidade => {
     const option = document.createElement('option');
-    option.setAttribute('disponibilidade-id', horario.id);
+    option.id = disponibilidade.disponibilidadeId
     option.value = `${disponibilidade.horaInicio} - ${disponibilidade.horaFim}`;
     option.textContent = `${disponibilidade.horaInicio} - ${disponibilidade.horaFim}`;
     horarioSelect.appendChild(option);
   });
 }
 
-
-medicoSelect.addEventListener('change', () => {
-    const cpf = medicoSelect.value;
-    if (cpf) 
-      carregarEspecialidades(cpf);
-    else 
-      especialidadeSelect.innerHTML = '<option value="">Selecione um médico primeiro</option>';
-});
-
-
 async function carregarResumo(cpf) {
   const token = sessionStorage.getItem("token")
+
   const response = await fetch(`http://localhost:8080/api/Consulta/ConfirmarConsultaDetalhes?cpf=${cpf}&nomeEspecialidade=${especialidadeSelect.value}`, {
     method: "GET",
     headers: {
@@ -101,50 +118,21 @@ async function carregarResumo(cpf) {
 
   if (response.status == 200) {
     const details = await response.json();
-    
-    medicoConsultaText.textContent = `Médico: ${medicoSelect.options[medicoSelect.selectedIndex].text}`;
-    especialidadeConsultaText.textContent = `Especialidade: ${especialidadeSelect.value}`;
-    dataConsultaText.textContent = `Data: ${dataInput.value}`;
-    horaConsultaText.textContent = `Horário: ${horarioSelect.value}`;
-    const valorConsulta = details.valorConsulta; 
-    const endereco = `${details.logradouro}, ${details.numero} ${details.complemento} - ${details.bairro}, ${details.municipio}, SP`;
-  
-    valorConsultaText.textContent = `Valor da Consulta: R$ ${valorConsulta}`;
-    enderecoConsultaText.textContent = `Endereço da Consulta: ${endereco}`;
+    document.getElementById('medico-consulta').textContent = `Médico: ${medicoSelect.options[medicoSelect.selectedIndex].text}`;
+    document.getElementById('especialidade-consulta').textContent = `Especialidade: ${especialidadeSelect.value}`;
+    document.getElementById('data-consulta').textContent = `Data: ${dataInput.value}`;
+    document.getElementById('horario-consulta').textContent = `Horário: ${horarioSelect.value}`;
+    document.getElementById('valor-consulta').textContent = `Valor da Consulta: R$ ${details.valorConsulta}`;
+    document.getElementById('endereco-consulta').textContent = isConsultaOnline.options[isConsultaOnline.selectedIndex].value == 1 ? "Consulta online via Google Meet" : `Endereço da Consulta: ${details.logradouro}, ${details.numero} ${details.complemento} - ${details.bairro}, ${details.municipio}, SP`;
   }
   else {
       window.location.href = "http://127.0.0.1:5500/html/login.html"
   }
 }
-    
-
-voltarButton.addEventListener('click', () => {
-  resumoConsultaDiv.classList.remove('form-active');
-  formSelecao.classList.add('form-active');
-});
-
-
-avancarButton.addEventListener('click', () => {
-  if(!especialidadeSelect.value || !medicoSelect.value || !dataInput.value || !horarioSelect.value){
-    alert("Insira todos os campos!");
-    return;
-  }
-  const cpf = medicoSelect.value;
-
-  if (cpf) {
-    carregarResumo(cpf);
-    formSelecao.classList.remove('form-active');
-    resumoConsultaDiv.classList.add('form-active');
-  }
-});
-
 
 dataInput.addEventListener('change', () => {
   const cpf = medicoSelect.value;
   const data = dataInput.value;
-  dataInput.min = dataAtual;
-  
-  console.log(cpf + " e " + data)
 
   if (cpf && data) {
     carregarDisponibilidades(cpf, data);
@@ -155,22 +143,23 @@ dataInput.addEventListener('change', () => {
 
 resumoConsulta.addEventListener('submit', async (event) => {
   event.preventDefault()
+  const token = sessionStorage.getItem("token")
 
   const data = {
-    disponibilidadeId: horarioSelect.options[horarioSelect.selectedIndex].getAttribute('disponibilidade-id'),
-    especialidaeId: especialidadeSelect.option[especialidadeSelect.selectedIndex].getAttribute('especialidade-id'),
-    situacao: "Agendada",
-    descricao: "Teste TESTE",
-    cpfPaciente: sessionStorage.getItem("email"),
-    isTelemedicina: false
+    disponibilidadeId: parseInt(horarioSelect.options[horarioSelect.selectedIndex].id),
+    especialidadeId: parseInt(especialidadeSelect.options[especialidadeSelect.selectedIndex].id),
+    descricao: descricaoInput.value,
+    emailPaciente: sessionStorage.getItem("email"),
+    isTelemedicina: isConsultaOnline.options[isConsultaOnline.selectedIndex].value == 1 ? true : false
   }
 
   const response = await fetch("http://localhost:8080/api/Consulta/Agendar", {
-    method: "GET",
+    method: "POST",
     headers: {
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json' // Adicionado cabeçalho Content-Type
     },
-    body: data
+    body: JSON.stringify(data) // Converte o objeto para JSON
   })
 
   if(response.status == 401) {
@@ -186,3 +175,10 @@ resumoConsulta.addEventListener('submit', async (event) => {
   }
 
 });
+
+function retornaDataFormatada(data) {
+  const ano = data.getFullYear();
+  const mes = String(data.getMonth() + 1).padStart(2, '0'); // Meses são baseados em zero
+  const dia = String(data.getDate()).padStart(2, '0');
+  return `${ano}-${mes}-${dia}`;
+}
