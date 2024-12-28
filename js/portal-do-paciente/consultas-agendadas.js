095688570fd8080526df84a60895e15d3d169c04
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-28 20:49:00 -0300

    add: pop-up das consultas agendadas

--- js/portal-do-paciente/consultas-agendadas.js
+++ js/portal-do-paciente/consultas-agendadas.js
@@ -3,7 +3,37 @@ let currentDate = new Date();
 
 
 document.addEventListener('DOMContentLoaded', async () => {
+    await fetchItems();
+
+    const profileContainer = document.getElementById("profile-container");
+    const profileMenu = document.getElementById("profile-menu");
+    const logoutOption = document.getElementById("logout");
+    const viewProfileOption = document.getElementById("view-profile");
+
+    document.getElementById("foto-perfil-options").addEventListener("click", () => {
+        profileMenu.style.display = profileMenu.style.display === "block" ? "none" : "block";
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!profileContainer.contains(event.target)) {
+            profileMenu.style.display = "none";
+        }
+    });
+
+    logoutOption.addEventListener("click", () => {
+        sessionStorage.clear();
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    });
+
+    viewProfileOption.addEventListener("click", () => {
+        window.location.href = "./meu-perfil.html";
+    });
+});
+
+
+async function fetchItems() {
     const token = sessionStorage.getItem("token")
+    const email = sessionStorage.getItem("email")
     
     const acesso = await fetch('http://localhost:8080/api/Consulta/Acessar', {
         method: 'GET',
@@ -20,13 +50,24 @@ document.addEventListener('DOMContentLoaded', async () => {
         Authorization: `Bearer ${token}`
         }
     });
+
+    const InfoBasicasUsuario = await fetch(`http://localhost:8080/api/Paciente/InfoBasicasUsuario?email=${email}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
     
     consultasPaciente = await response.json();
+
+    const InfoBasicasUsuarioJson = await InfoBasicasUsuario.json();
+    document.getElementById("foto-perfil-options").src = InfoBasicasUsuarioJson.fotoPerfilUrl == "" ? "../../assets/foto-medicos-teste/vetor-de-ícone-foto-do-avatar-padrão-símbolo-perfil-mídia-social-sinal-259530250.webp" : InfoBasicasUsuarioJson.fotoPerfilUrl;
 
     currentDateHTML.textContent = currentDate.toLocaleDateString();
 
     mostrarConsultas();
-});
+}
 
 document.getElementById("prev-day").addEventListener('click', () => {
     currentDate.setDate(currentDate.getDate() - 1);
@@ -55,7 +96,7 @@ function mostrarConsultas() {
         console.log(consultasPaciente);
 
         const consultasDoDia = consultasPaciente.filter(consulta => {
-            return consulta.dataConsulta == currentDateHTML.textContent;
+            return formatarData(consulta.dataConsulta) == currentDateHTML.textContent;
         });
 
         if (consultasDoDia.length > 0) {
@@ -63,6 +104,9 @@ function mostrarConsultas() {
 
                 const divCard = document.createElement("div");
                 divCard.className = "div-card";
+
+                divCard.setAttribute('consulta-identificador', consulta.consultaId);
+
 
                 const fotoMedico = document.createElement("img");
                 fotoMedico.src = consulta.fotoPerfilUrl;
@@ -91,6 +135,10 @@ function mostrarConsultas() {
                 divCard.appendChild(status);
                 divCard.appendChild(horario);
 
+                divCard.addEventListener('click', () => {
+                    const consultaIdentificador = divCard.getAttribute('consulta-identificador');
+                    showPopup(consultaIdentificador);
+                });
 
                 consultas.appendChild(divCard);
             });
@@ -112,4 +160,65 @@ function mostrarConsultas() {
         consultas.classList.add('fade-in');
 
     }, 200); // Tempo do efeito de saída
+}
+
+
+async function showPopup(consultaIdentificador) {
+    try {
+
+      const consulta = consultasPaciente.find(consulta => consulta.consultaId == consultaIdentificador);
+
+      const fotoPerfil = document.getElementById("fotoPerfil");
+      const nomeMedico = document.getElementById("nomeMedico");
+      const dataConsulta = document.getElementById("dataConsulta");
+      const horario = document.getElementById("horario");
+    //   const motivo = document.getElementById("motivo");
+      const preco = document.getElementById("preco");
+      const endereco = document.getElementById("endereco");
+      const especialidade = document.getElementById("especialidade");
+
+      // Preenchendo as informações no pop-up
+      fotoPerfil.src = consulta.fotoPerfilUrl;
+      nomeMedico.textContent = consulta.nomeMedico
+      dataConsulta.textContent = `Data da consulta: ${formatarData(consulta.dataConsulta)}`;
+      horario.textContent = `Horário: ${consulta.horaInicio} - ${consulta.horaFim}`;
+      especialidade.textContent = `Especialidade: ${consulta.especialidade}`;
+      motivo.textContent = `Motivo da consulta: ${consulta.descricao}`;
+      preco.textContent = `Preço: R$ ${consulta.preco}`;
+
+      if(!consulta.isTelemedicina) 
+        endereco.textContent = `Endereço: ${consulta.logradouro}, ${consulta.numero} - ${consulta.bairro} - ${consulta.municipio} - ${consulta.uf}, ${consulta.cep}`;
+      else 
+        endereco.textContent = `Consulta Online via Google Meet`;
+
+      popupOverlay.style.display = "flex";
+
+      popupOverlay.setAttribute('medico-identificador', nomeMedico.textContent);
+    } catch (error) {
+      console.error(error.message);
+      alert("Não foi possível carregar as informações do médico.");
+    }
+}
+
+
+// Evento para fechar o pop-up
+closePopup.addEventListener("click", () => {
+popupOverlay.style.display = "none";
+});
+
+
+
+// Redirecionar para agendar consulta
+document.getElementById("cancelarConsulta").addEventListener("click", async () => {
+    const token = sessionStorage.getItem("token");
+
+    alert(token)
+   
+});
+
+
+
+
+function formatarData (data) {
+    return data.split('-').reverse().join('/');
 }
